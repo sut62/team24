bfdba949f5e74644c53b2df8cb7652aaebe29cf4
ToeiKanta <41673034+ToeiKanta@users.noreply.github.com>
2019-12-15 01:24:12 +0700

    add Search page

--- client/src/components/BookFlight/Search.vue
+++ client/src/components/BookFlight/Search.vue
@@ -1,8 +1,47 @@
 <template>
   <div>
-    <h1>ค้นหาเที่ยวบิน</h1>
-    <v-text-field @click="showDatePick =! showDatePick" v-model="dateRangeText" label="เลือกวันเวลาไป-กลับ" readonly></v-text-field>
-    <v-date-picker v-if="showDatePick" v-model="dates" range></v-date-picker>
+    <h3 class="text-white">ค้นหาเที่ยวบิน</h3>
+    <div class="card text-center">
+      <div class="card-header">
+        <ul class="nav nav-tabs card-header-tabs">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">เที่ยวบิน</a>
+          </li>
+        </ul>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">เริ่มต้นการเดินทาง</h5>
+        <v-row>
+          <v-col cols="2">
+            <v-autocomplete
+              v-model="select"
+              :items="items"
+              outlined
+              label="จาก"
+              filled
+            ></v-autocomplete>
+          </v-col>
+          <v-col cols="2">
+            <v-autocomplete
+              v-model="select"
+              :items="items"
+              outlined
+              label="ถึง"
+              filled
+            ></v-autocomplete>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field @click="showDatePick =! showDatePick" outlined v-model="dateRangeText" label="เลือกวันเวลาไป-กลับ" readonly></v-text-field>
+            <v-date-picker v-if="showDatePick" class="date-picker" v-model="dates" range></v-date-picker>
+          </v-col>
+          <v-col cols="4">
+            <a href="#" class="btn btn-danger text-white btn-lg" style="width:100%;">ค้นหา</a>
+          </v-col>
+        </v-row>
+      </div>
+    </div>
+    <hr/>
+    
   </div>
 </template>
 
@@ -14,6 +53,13 @@ export default {
   data: () => ({
     showDatePick: false,
     dates: ['2019-09-10', '2019-09-20'],
+    select: 'Programming',
+    items: [
+      'กรุงเทพ',
+      'ขอนแก่น',
+      'ภูเก็ต',
+      'เลย',
+    ],
   }),
   computed: {
     dateRangeText () {
@@ -22,3 +68,12 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+  .date-picker{
+    position: absolute;
+    left: 20px;
+    top: 70px;
+    width: 500px;
+  }
+</style>
--- client/src/views/BookFlight.vue
+++ client/src/views/BookFlight.vue
@@ -1,19 +1,70 @@
 <template>
   <div class="bookFlight">
-    <nav class="navbar navbar-dark bg-white">
-      <!-- Navbar content -->
-      <button class="btn-warning btn">จองเที่ยวบิน</button>
-    </nav>
-
-    <div class="container">
-      <div class="mx-auto" style="width: 400px;">
-        <div class="topic">
-          <h1 class="mt-5">เริ่มทริปของคุณที่นี่</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
+        <div class="my-2 my-lg-0">
+          <button class="btn">เริ่มจองเที่ยวบิน</button>
+          <button class="btn">โปรโมชั่น</button>
+          <button class="btn">เช็คอิน</button>
+          <button class="btn mr-5">ชำระค่าเที่ยวบิน</button>
+          <button class="btn my-2 my-sm-0" type="submit">เข้าสู่ระบบ | สมัครสมาชิก</button>
         </div>
       </div>
-      <Search/>
+    </nav>
+    <div class="header-bg"></div>
+    
+    <div class=" container" style="margin-top:100px;">
+      <v-avatar class="logo" size="100"> 
+        <img
+          src="../assets/logo.png"
+          alt="John"
+        >
+      </v-avatar>
+      <div class="mx-auto">
+        <div class="my-content">
+          <div class="text-white" style="width: 400px;">
+            <div class="topic">
+              <h1 class="mt-5 topic">เริ่มทริปของคุณที่นี่</h1>
+            </div>
+          </div>
+          <Search/>
+        </div>
+      </div>
     </div>
     
+    <template>
+      <v-footer
+        absolute
+        color="primary lighten-1"
+        padless
+      >
+        <v-row
+          justify="center"
+          no-gutters
+        >
+          <v-btn
+            v-for="link in links"
+            :key="link"
+            color="white"
+            text
+            rounded
+            class="my-2"
+          >
+            {{ link }}
+          </v-btn>
+          <v-col
+            class="primary lighten-2 py-4 text-center white--text"
+            cols="12"
+          >
+            @ {{ new Date().getFullYear() }} — <strong>Airplane</strong>
+          </v-col>
+        </v-row>
+      </v-footer>
+    </template>
   </div>
 </template>
 
@@ -23,8 +74,45 @@ import Search from '../components/BookFlight/Search'
 
 export default {
   name: 'boookFlight',
+  data: ()=>({
+    links: [
+      'เริ่มจองเที่ยวบิน',
+      'เช็คอิน',
+      'ชำระค่าเที่ยวบิน',
+      'เข้าสู่ระบบ',
+      'สมัครสมาชิก',
+      'ติดต่อเรา',
+    ],
+  }),
   components: {
     Search
-  }
+  },
 }
 </script>
+
+<style scoped>
+  .header-bg{
+    height: 300px;
+    width: 100%;
+    background-size: cover;
+    /* background-color: rgb(8, 2, 43); */
+    background-image: url("../assets/head-bg.jpg");
+  }
+  .container{
+    padding-left: 100px;
+  }
+  .logo{
+    position: absolute;
+    top: 30px;
+  }
+  .my-right{
+    position: absolute;
+    right: 1px;
+  }
+  
+  .my-content{
+    position: absolute;
+    top: 150px;
+    width: 70%;
+  }
+</style>
