e6d771f9554bb03aed146e1ea325639744ce44be
ToeiKanta <41673034+ToeiKanta@users.noreply.github.com>
2019-12-14 23:12:02 +0700

    add BookFlight page

---
+++ client/src/components/BookFlight/Search.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>ค้นหาเที่ยวบิน</h1>
+    <v-text-field @click="showDatePick =! showDatePick" v-model="dateRangeText" label="เลือกวันเวลาไป-กลับ" readonly></v-text-field>
+    <v-date-picker v-if="showDatePick" v-model="dates" range></v-date-picker>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: 'search',
+
+  data: () => ({
+    showDatePick: false,
+    dates: ['2019-09-10', '2019-09-20'],
+  }),
+  computed: {
+    dateRangeText () {
+      return this.dates.join(' ~ ')
+    },
+  },
+};
+</script>
---
+++ client/src/views/BookFlight.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bookFlight">
+    <nav class="navbar navbar-dark bg-white">
+      <!-- Navbar content -->
+      <button class="btn-warning btn">จองเที่ยวบิน</button>
+    </nav>
+
+    <div class="container">
+      <div class="mx-auto" style="width: 400px;">
+        <div class="topic">
+          <h1 class="mt-5">เริ่มทริปของคุณที่นี่</h1>
+        </div>
+      </div>
+      <Search/>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Search from '../components/BookFlight/Search'
+
+export default {
+  name: 'boookFlight',
+  components: {
+    Search
+  }
+}
+</script>
