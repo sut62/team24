1e2e3f6a21f8966965ce186685ad1666f6696060
thisKK <42506615+thisKK@users.noreply.github.com>
2019-12-28 13:58:31 +0700

    Add entity checkintype - #22

---
+++ backend/src/main/java/com/cpe/team24/entity/CheckInType.java
@@ -0,0 +1,39 @@
+package entity;
+
+import lombok.*;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+
+@CrossOrigin(origins = "*")
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "CHECKINTYPE")
+public class CheckInType {
+
+    @Id
+    @SequenceGenerator(name = "CHECKINTYPE_SEQ", sequenceName = "CHECKINTYPE_SEQ")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CHECKINTYPE_SEQ")
+    @Column(name = "CHECKINTYPE_ID", unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = "TYPE",nullable = false)
+    private String type;
+
+    //entity checkin type
+    
+
+}
