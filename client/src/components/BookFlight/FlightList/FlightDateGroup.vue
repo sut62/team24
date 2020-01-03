3f1b69bb9ba42b0291a265a8fea316c6f6c7f35c
ToeiKanta <41673034+ToeiKanta@users.noreply.github.com>
2020-01-04 05:22:36 +0700

    Update prevent past date selected - #63

--- client/src/components/BookFlight/FlightList/FlightDateGroup.vue
+++ client/src/components/BookFlight/FlightList/FlightDateGroup.vue
@@ -3,15 +3,15 @@
     <v-row justify="space-around">
       <div>
         <v-icon>mdi-chevron-left-box</v-icon>
-      <button @click="getFlight(-3)" class="mybtn btn btn-primary  text-center">{{ getDate(-3) | moment("dd")  }}<br> {{ getDate(-3) | moment("DD MMMM")  }}</button>
+      <button @click="getFlight(-3)"  :disabled="!dateCheck(-3)" :class="{'mybtn btn btn-primary text-center':dateCheck(-3),'mybtn-disable btn btn-secondary text-center':!dateCheck(-3)}">{{ getDate(-3) | moment("dd")  }}<br> {{ getDate(-3) | moment("DD MMMM")  }}</button>
       </div>
-      <button @click="getFlight(-2)" class="mybtn btn btn-primary text-center">{{ getDate(-2) | moment("dd")  }}<br> {{ getDate(-2) | moment("DD MMMM")  }}</button>
-      <button @click="getFlight(-1)" class="mybtn btn btn-primary text-center">{{ getDate(-1) | moment("dd")  }}<br> {{ getDate(-1) | moment("DD MMMM")  }}</button>
-      <button @click="getFlight(0)" class="selected btn btn-primary text-center">{{ getDate(0) | moment("dd") }} <br> {{ getDate(0) | moment("DD MMMM")  }}</button>
-      <button @click="getFlight(1)" class="mybtn btn btn-primary text-center">{{ getDate(1) | moment("dd")  }}<br> {{ getDate(1) | moment("DD MMMM")  }}</button>
-      <button @click="getFlight(2)" class="mybtn btn btn-primary text-center">{{ getDate(2) | moment("dd")  }}<br> {{ getDate(2) | moment("DD MMMM")  }}</button>
+      <button @click="getFlight(-2)"  :disabled="!dateCheck(-2)" :class="{'mybtn btn btn-primary text-center':dateCheck(-2),'mybtn-disable btn btn-secondary text-center':!dateCheck(-2)}">{{ getDate(-2) | moment("dd")  }}<br> {{ getDate(-2) | moment("DD MMMM")  }}</button>
+      <button @click="getFlight(-1)"  :disabled="!dateCheck(-1)" :class="{'mybtn btn btn-primary text-center':dateCheck(-1),'mybtn-disable btn btn-secondary text-center':!dateCheck(-1)}">{{ getDate(-1) | moment("dd")  }}<br> {{ getDate(-1) | moment("DD MMMM")  }}</button>
+      <button @click="getFlight(0)" :disabled="!dateCheck(0)"  :class="{'selected btn btn-primary text-center':dateCheck(0),'selected btn btn-primary text-center':dateCheck(0)}">{{ getDate(0) | moment("dd") }} <br> {{ getDate(0) | moment("DD MMMM")  }}</button>
+      <button @click="getFlight(1)"  :disabled="!dateCheck(1)" :class="{'mybtn btn btn-primary text-center':dateCheck(1),'mybtn-disable btn btn-secondary text-center':!dateCheck(1)}">{{ getDate(1) | moment("dd")  }}<br> {{ getDate(1) | moment("DD MMMM")  }}</button>
+      <button @click="getFlight(2)"  :disabled="!dateCheck(2)" :class="{'mybtn btn btn-primary text-center':dateCheck(2),'mybtn-disable btn btn-secondary text-center':!dateCheck(2)}">{{ getDate(2) | moment("dd")  }}<br> {{ getDate(2) | moment("DD MMMM")  }}</button>
       <div>
-      <button @click="getFlight(3)" class="mybtn btn btn-primary text-center">{{ getDate(3) | moment("dd")  }}<br> {{ getDate(3) | moment("DD MMMM")  }}</button>
+      <button @click="getFlight(3)"  :disabled="!dateCheck(3)" :class="{'mybtn btn btn-primary text-center':dateCheck(3),'mybtn-disable btn btn-secondary text-center':!dateCheck(3)}">{{ getDate(3) | moment("dd")  }}<br> {{ getDate(3) | moment("DD MMMM")  }}</button>
       <v-icon>mdi-chevron-right-box</v-icon>
       </div>
     </v-row>
@@ -34,7 +34,7 @@ export default {
     ...mapState({
       'departDate': state => state.BookFlight.data.departDate,
       'returnDate': state => state.BookFlight.data.returnDate,
-    })
+    }),
   },
   methods:{
     ...mapActions({
@@ -64,6 +64,25 @@ export default {
         this.getFlightDepart(date)
       }else{
         this.getFlightReturn(date)
+      }
+    },
+    dateCheck(count){
+      if(this.isDepart){
+        let date = new Date(this.departDate)
+        if(date.setDate(date.getDate() + count ) - new Date() < 0){
+          return false
+        }else{
+          return true;
+        }
+      }
+      let returnDate = new Date(this.returnDate)
+      returnDate.setDate(returnDate.getDate() + count)
+      let diffTime = (new Date(returnDate) - new Date(this.departDate))
+
+      if(diffTime < 0){
+        return false
+      }else{
+        return true
       }
     }
   },
@@ -98,5 +117,15 @@ export default {
     border-style: solid;
     border-color: white;
     border-width: 1px;
+  }
+  .mybtn-disable{
+    height: 80px;
+    width: 150px;
+    border-radius: 10px;
+    background-color: rgb(216, 216, 216);
+    border-style: solid;
+    border-color: white;
+    border-width: 1px;
+    padding: 10px;
   }
 </style>
